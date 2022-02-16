213201a4455febb9f8a813c4103aa8a23b9796a3
caitevanish <cait.evan@gmail.com>
2022-02-16 09:34:19 -0800

    Updated settings.py. Created and migrated Song model to MySQL workbench. REgistered model and fixed line 3. Tested in server; added 4 songs to database from admin site.

--- musiclibrary/music/models.py
+++ musiclibrary/music/models.py
@@ -1,3 +1,11 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+  title = models.CharField(max_length=50)
+  artist = models.CharField(max_length=50)
+  album = models.CharField(max_length=50)
+  genre = models.CharField(max_length=50)
+  release_date = models.DateField()
+  
